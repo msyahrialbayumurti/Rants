af90ba1d6fdfa287c48d9a9ef8ed2508dd3f6ab8
Muhammadyafiheldy <m.yafiheldy@gmail.com>
2024-12-14 02:47:28 +0700

    api kostum

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\GalleryController; 
-use App\Http\Controllers\GalleryController;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
@@ -45,8 +44,8 @@ route::get('/acara/{tanngal}',[CalendarController::class, 'schedule' ]);
 Route::get('galleries/',[GalleryController::class,'index']);
 
 //Kostum
-Route::prefix('kostum')->group(function () {
-    Route::get('/all', [KostumController::class, 'index']); // Mendapatkan semua kostum
+Route::prefix('api')->group(function () {
+    Route::get('/kostum/', [KostumController::class, 'index']); // Mendapatkan semua kostum
 //     Route::post('/', [KostumController::class, 'store']); // Menambahkan kostum
 //     Route::get('/{id}', [KostumController::class, 'show']); // Mendapatkan detail kostum
 //     Route::put('/{id}', [KostumController::class, 'update']); // Mengupdate kostum
